27e93db6e8f03ea6de1694dc44bfa3fe7a833db0
Matheus Glauber de Araujo Ribeiro <matheus.araujo@softwillians.com.br>
2022-08-29 22:38:29 -0300

    Configurado registro e acesso de usuario de forma basica

---
+++ Fit.HealthCareApi.Application/ViewModel/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Fit.HealthCareApi.Application.ViewModel
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "Os campos de senha não conferem")]
+        public string ConfirmarSenha { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+    }
+}
--- Gigers.GigersApi/V1/Controllers/AuthController.cs
+++ Gigers.GigersApi/V1/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
 ﻿using Fit.HealthCareApi.Application.Intefaces;
+using Fit.HealthCareApi.Application.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,63 @@ using System.Threading.Tasks;
 
 namespace Fit.HealthCareApi.V1.Controllers
 {
+    [Route("api/conta/")]
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
-        {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador) 
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registrarUsuario)
+        {
+            if (!ModelState.IsValid) 
+                return Ok(new Exception("Modelo de registro não é válido"));
+
+            IdentityUser _user = new IdentityUser
+            {
+                Email = registrarUsuario.Email,
+                UserName = registrarUsuario.Email,
+                EmailConfirmed = true
+            };
+
+            var _resultado = await _userManager.CreateAsync(_user, registrarUsuario.Senha);
+
+            if (_resultado.Succeeded)
+            {
+                await _signInManager.SignInAsync(_user, false);
+                return Ok(registrarUsuario);
+            }
+
+            foreach (var erro in _resultado.Errors)
+            {
+                NotificarErro(erro.Description);
+            }
+
+            return Ok(registrarUsuario);
+        }
+
+        [HttpPost("entrar")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUsuario)
+        {
+            if (!ModelState.IsValid)
+                return Ok(new Exception("Modelo de registro não é válido"));
+
+            var _resultado = await _signInManager.PasswordSignInAsync(loginUsuario.Email, loginUsuario.Senha, false, false);
+
+            if (_resultado.Succeeded)
+                return Ok(loginUsuario);
+
+            NotificarErro("Usuário ou senha incorreto");
+
+            return Ok(loginUsuario);
         }
     }
 }
